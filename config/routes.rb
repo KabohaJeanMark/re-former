768b1d2fb4ad9c0fdfb57f9ab181a307bedaf47a
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-05-28 16:33:28 +0300

    Add CRUD method to Read. Read all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resource :users, only: [:new, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users
+
+  root "users#index"
+
+  get "/users/:id", to: "users#show"
 end
