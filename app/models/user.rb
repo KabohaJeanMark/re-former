fcf42dc7a8791034436a7c126e6ba7c5f85643a6
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-05-28 14:12:24 +0300

    Add validations for the presence of each field and uniqueness for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
 end
