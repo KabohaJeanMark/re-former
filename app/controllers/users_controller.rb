ada071b6dba8ecd8b66a941e363ebd5912eea45d
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-05-31 10:23:30 +0300

    Remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # using the private params method
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password]) # the old way
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
